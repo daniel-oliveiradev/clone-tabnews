9589a8ad8a8a03f199d2c9d6a228c5b21c171923
Daniel Oliveira <124593963+daniel-oliveiradev@users.noreply.github.com>
2024-12-19 14:33:02 +0000

    adicionando o prettier no projeto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
-function Home(){
-    return <h1>Você é especial, mesmo fazendo as coisas que já falei pra você não fazer S2</h1>
+function Home() {
+  return (
+    <h1>
+      Você é especial, mesmo fazendo as coisas que já falei pra você não fazer
+      S2
+    </h1>
+  );
+}
+
+function test() {
+  console.log("teste");
 }
 
 export default Home;
